2b9c43cb7b49a3b7cdc5d720241090a15c44500b
rnjava <rnjava@gmail.com>
2019-04-19 19:08:58 -0700

    After completing React 0

---
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./types";
+
+export const createProject = (project, history) => async dispatch => {
+  try {
+    const res = await axios.post("/api/project", project);
+    history.push("/dashboard");
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: {}
+    });
+  }
+};
+
+export const getProjects = () => async dispatch => {
+  const res = await axios.get("/api/project/all");
+  dispatch({
+    type: GET_PROJECTS,
+    payload: res.data
+  });
+};
+
+export const getProject = (id, history) => async dispatch => {
+  try {
+    const res = await axios.get(`/api/project/${id}`);
+    dispatch({
+      type: GET_PROJECT,
+      payload: res.data
+    });
+  } catch (error) {
+    history.push("/dashboard");
+  }
+};
+
+export const deleteProject = id => async dispatch => {
+  if (
+    window.confirm(
+      "Are you sure? This will delete the project and all the tasks related with it"
+    )
+  ) {
+    await axios.delete(`/api/project/${id}`);
+    dispatch({
+      type: DELETE_PROJECT,
+      payload: id
+    });
+  }
+};
